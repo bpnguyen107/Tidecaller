6608ae6ed6fbaa0cd7d1c9ed8528d78cd19a402b
Dane Guthner <dane.guthner@yahoo.com>
2023-03-04 13:44:41 -0800

    Installed expo location and react-native-maps to MapScreen.js

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View, Text } from 'react-native';
 
 const MapScreen = ({ navigation }) => {
+  /*
+  const [position, setPosition] = useState<Location.LocationObject | null>(null);
+
+  useEffect(() => {
+    const requestPermissions = async () => {
+        const foreground = await Location.requestForegroundPermissionsAsync();
+        if (foreground.granted) {
+            const { granted } = await Location.getForegroundPermissionsAsync();
+            if (!granted) {
+                console.log('location tracking denied');
+                return;
+            }
+            const foregroundSubscription = await Location.watchPositionAsync(
+                {
+                    // For better logs, we set the accuracy to the most sensitive option
+                    accuracy: Location.Accuracy.BestForNavigation,
+                },
+                (location) => {
+                    if (location !== null) {
+                        setPosition(location);
+                    }
+                }
+            );
+        }
+    };
+    requestPermissions();
+}, []); */
+
+
   return (
     <View style={styles.container}>
-      <Text>Map Screen</Text>
+      <MapView
+      style={styles.map}
+      region={{
+        latitude: 34.06935,
+        longitude: -118.44468,
+        latitudeDelta: 0.020,
+        longitudeDelta: 0.020,
+      }}
+    />
     </View>
   );
 }
@@ -16,6 +55,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  }
 });
 
 export default MapScreen;
