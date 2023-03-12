cb304636bdec4e8e8a23fd9a3c183e43ac52c9d3
Sebastian Caid <scaid@tangocode.com>
2023-03-11 18:02:58 -0800

    Added search bar and pin dropping capabilities.
    Using the react-native-google-places-autocomplete package, we were
    able to add a search bar that displays places near you based on the
    search query. We used the Google Maps API to do this. We also
    enabled the capability to drop a pin on the search location.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,59 +1,106 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Dimensions, StyleSheet, View, Text } from 'react-native';
+import MapView, { Callout, Marker } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import * as Location from 'expo-location';
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, View, Text } from 'react-native';
 
-const MapScreen = ({ navigation }) => {
+export default function App() {
 
-  const [loc, setLoc] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [pin, setPin] = useState({latitude: 37.78825, longitude: -122.4324});
+  const [region, setRegion] = useState({
+    latitude: 37.78825, 
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+  });
 
   useEffect(() => {
-    console.log('something');
-    const requestPermissions = async () => {
-        const foreground = await Location.requestForegroundPermissionsAsync();
-        if (foreground.granted) {
-            const { granted } = await Location.getForegroundPermissionsAsync();
-            if (!granted){
-              console.log('Tracking Denied');
-              return;
-            }
-        const position = await Location.watchPositionAsync(
-            {
-                // For better logs, we set the accuracy to the most sensitive option
-                accuracy: Location.Accuracy.BestForNavigation,
-            },
-            (location) => {
-              console.log('location null');
-              setLoc(location);
-            }
-          );
-        }
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log("Please grant location permissions");
+        return;
+      }
+  
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
     };
+    getPermissions();
+  }, [])
 
-    requestPermissions();
+  let currentLat = 0
+  let currentLng = 0
 
-  }, []);
-
-  let latitude_real = 0;
-  let longitude_real = 0;
-  if (loc !== null) {
-      latitude_real = loc.coords.latitude;
-      longitude_real = loc.coords.longitude;
-  } else {
-      //loading view
+  if (location) {
+    currentLat = location.coords.latitude
+    currentLng = location.coords.longitude
   }
-  console.log(latitude_real);
+
   return (
-    <View style={styles.container}>
+      <View style={{ marginTop: 50, flex: 1 }}>
+        <GooglePlacesAutocomplete
+          placeholder='Search'
+          fetchDetails={true}
+          GooglePlacesSearchQuery={{
+            rankby: "distance"
+          }}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+            setRegion({
+              latitude: details.geometry.location.lat,
+              longitude: details.geometry.location.lng,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
+            });
+          }}
+          query={{
+            key: 'AIzaSyCtlqDstZqTuGiimjz5bOggecVpbILC5Ko',
+            language: 'en',
+            components: "country:us",
+            types: "establishment",
+            radius: 30000,
+            location: `${currentLat}, ${currentLng}`
+          }}
+          styles={{
+            container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
+            listView: { backgroundColor: "white" }
+          }}
+        ></GooglePlacesAutocomplete>
       <MapView
-      style={styles.map}
-      region={{
-        latitude: latitude_real,
-        longitude: longitude_real,
-        latitudeDelta: 0.020,
-        longitudeDelta: 0.020,
-      }}
-    />
+        style={styles.map} 
+        region={{
+          latitude: currentLat,
+          longitude: currentLng,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider="google"
+      >
+        <Marker coordinate={{
+          latitude: region.latitude, 
+          longitude: region.longitude}}
+        ></Marker>
+        <Marker
+          coordinate={pin}
+          pinColor="blue"
+          draggable={true}
+          onDragStart={(e) => {
+            console.log("Drag start", e.nativeEvent.coordinates)
+          }}
+          onDragEnd={(e) => {
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude
+            })
+          }}
+        >
+          <Callout>
+            <Text>I'm here</Text>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -61,13 +108,12 @@ const MapScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
   }
 });
-
-export default MapScreen;
