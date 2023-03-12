22f18870a29695ff9a07a71f9e5d871c621a7b94
Dane Guthner <dane.guthner@yahoo.com>
2023-03-11 19:51:46 -0800

    fetches api data from the different station api

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,10 +3,22 @@ import * as Location from 'expo-location';
 import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View, Text, Animated } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { StackActions } from '@react-navigation/native';
 
 const MapScreen = ({ navigation }) => {
 
   const [loc, setLoc] = useState(null);
+  const [station, setStation] = useState([]);
+  const [caStation, setCaStation] = useState([]);
+
+  const fetchStation = async () => {
+    const response = await fetch(
+     'https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json?type=tidepredictions&units=english'
+    );
+    const data = await response.json();
+      setStation(data.stations);
+  }
+
 
   useEffect(() => {
     const requestPermissions = async () => {
@@ -30,6 +42,7 @@ const MapScreen = ({ navigation }) => {
     };
 
     requestPermissions();
+    fetchStation();
 
   }, []);
 
