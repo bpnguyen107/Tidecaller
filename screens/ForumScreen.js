8e009193a75c1cde7e8b16aa7a3dce8c0e39767a
Jennie Pham <jenniepham14@gmail.com>
2023-03-14 01:49:19 -0700

    styled forum/feed interface

--- screens/ForumScreen.js
+++ screens/ForumScreen.js
@@ -10,20 +10,42 @@ import {
   View,
   Button
 } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import { getFirestore, doc, setDoc, getDocs, onSnapshot, orderBy,
-  addDoc, collection, updateDoc, query, where, limit } 
+import { getFirestore, onSnapshot, orderBy, collection, query } 
 from 'firebase/firestore';
 
-const Item = ({item, onPress, backgroundColor, textColor}) => (
-  <View onPress={onPress} style={[styles.item, {backgroundColor}]}>
-    <Image style={{width: '10%', height: '10%'}} source={{uri: item.profilePic ? item.profilePic : "https://cdn.discordapp.com/attachments/1067598393402200086/1085075999810670692/image.png"}}/>
-    <Text style={[styles.title, {color: textColor}]}>{item.username ? item.username : "SysError"}</Text>
-    <Image style={{width: '70%', height: '70%'}} source={{uri: item.imageUri ? item.imageUri : "https://cdn.discordapp.com/attachments/1067598393402200086/1085075999810670692/image.png"}}/> 
-    <Text style={[styles.title, {color: textColor}]}>{item.message}</Text>
-    <Text> Uploaded: {item.uploadTime ? item.uploadTime : "Egging Time"}</Text>
+const Item = ({item, onPress, textColor}) => (
+  <View onPress={onPress} style={[styles.item, {backgroundColor :'#183645'}]}>
+    
+  <View style={{flexDirection: 'row', marginTop: 15, marginBottom: 15}}>
+    <Image
+      style={{width: '12%', aspectRatio:1, marginLeft: 20, marginRight: 10, borderRadius: 50}}
+      source={{uri: item.profilePic ? item.profilePic : "https://cdn.discordapp.com/attachments/1067598393402200086/1085075999810670692/image.png"}}
+    />
+  <View>
+    <Text 
+      style={[styles.title, {color: textColor, fontWeight: '475'}]}>
+      {item.username ? item.username : "SysError"}
+    </Text>
+
+    <Text style={{color:'#C7C7CC'}}>
+       Posted on {item.uploadTime ? item.uploadTime : "Egging Time"}
+    </Text>
+    </View>
+    </View>
+    <Image
+     style={{width: '100%', aspectRatio: 3/2}}
+     source={{uri: item.imageUri ? item.imageUri : "https://cdn.discordapp.com/attachments/1067598393402200086/1085075999810670692/image.png"}}
+    />
+
+    <Text 
+      style={[styles.title, {color: textColor, marginBottom: 10, marginTop: 10, marginLeft: 15, marginRight: 15, fontSize: 20}]}>
+      {item.message}
+    </Text>
+
+  
   </View>
 );
+
 
 const ForumScreen = () => {
   const [selectedId, setSelectedId] = useState();
@@ -39,22 +61,19 @@ const ForumScreen = () => {
       setItems(postData);
     })
     }, [])
-  
-  //console.log(items)
 
   const renderItem = ({item}) => {
     const backgroundColor = item.id === selectedId ? '#6495ed' : '#6495ed';
-    const color = item.id === selectedId ? 'black' : 'black';
-  
-  
-  
-
+    const color = item.id === selectedId ? '#D9DBE0' : '#D9DBE0';
+    
     return (
+     
       <Item
         item={item}
         backgroundColor={backgroundColor}
         textColor={color}
       />
+     
     );
   };
 
@@ -75,8 +94,9 @@ const ForumScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor:"#0A1419",
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0, //?
+    marginTop: StatusBar.currentHeight || 0, 
   },
   popularItems: {
     borderWidth: 0
@@ -85,8 +105,7 @@ const styles = StyleSheet.create({
     padding: 2,
     marginVertical: 8,
     marginHorizontal: 0,
-    height: 700,
-    borderRadius: 9,
+    borderRadius: 5,
     alignContent: 'center',
   },
   title: {
