aa804746d325bbab432750ce775dad10661ce76f
bpnguyen107 <bpnguyen107@g.ucla.edu>
2023-03-14 18:09:02 -0700

    category name shows up on post

--- screens/ForumScreen.js
+++ screens/ForumScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import {
   FlatList,
   SafeAreaView,
@@ -10,39 +10,47 @@ import {
   View,
   Button
 } from 'react-native';
-import { getFirestore, onSnapshot, orderBy, collection, query } 
-from 'firebase/firestore';
+import { getFirestore, onSnapshot, orderBy, collection, query }
+  from 'firebase/firestore';
 
-const Item = ({item, onPress, textColor}) => (
-  <View onPress={onPress} style={[styles.item, {backgroundColor :'#183645'}]}>
-    
-  <View style={{flexDirection: 'row', marginTop: 15, marginBottom: 15}}>
-    <Image
-      style={{width: '12%', aspectRatio:1, marginLeft: 20, marginRight: 10, borderRadius: 50}}
-      source={{uri: item.profilePic ? item.profilePic : "https://cdn.discordapp.com/attachments/1067598393402200086/1085075999810670692/image.png"}}
-    />
-  <View>
-    <Text 
-      style={[styles.title, {color: textColor, fontWeight: '475'}]}>
-      {item.username ? item.username : "SysError"}
-    </Text>
+const Item = ({ item, onPress, textColor }) => (
+  <View onPress={onPress} style={[styles.item, { backgroundColor: '#183645' }]}>
 
-    <Text style={{color:'#C7C7CC'}}>
-       Posted on {item.uploadTime ? item.uploadTime : "Egging Time"}
-    </Text>
+    <View style={{ flexDirection: 'row', marginTop: 15, marginBottom: 15 }}>
+      <Image
+        style={{ width: '12%', aspectRatio: 1, marginLeft: 20, marginRight: 10, borderRadius: 50 }}
+        source={{ uri: item.profilePic ? item.profilePic : "https://cdn.discordapp.com/attachments/1067598393402200086/1085075999810670692/image.png" }}
+      />
+      <View>
+        <Text
+          style={[styles.title, { color: textColor, fontWeight: '475' }]}>
+          {item.username ? item.username : "SysError"}
+        </Text>
+
+        <Text style={{ color: '#C7C7CC' }}>
+          Posted on {item.uploadTime ? item.uploadTime : "Egging Time"}
+        </Text>
+      </View>
     </View>
-    </View>
     <Image
-     style={{width: '100%', aspectRatio: 3/2}}
-     source={{uri: item.imageUri ? item.imageUri : "https://cdn.discordapp.com/attachments/1067598393402200086/1085075999810670692/image.png"}}
+      style={{ width: '100%', aspectRatio: 3 / 2 }}
+      source={{ uri: item.imageUri ? item.imageUri : "https://cdn.discordapp.com/attachments/1067598393402200086/1085075999810670692/image.png" }}
     />
-
-    <Text 
-      style={[styles.title, {color: textColor, marginBottom: 10, marginTop: 10, marginLeft: 15, marginRight: 15, fontSize: 20}]}>
+    <Text
+      style={{
+        color: textColor,
+        marginLeft: 15,
+        marginTop: 10,
+      }}
+    >
+      Category: {item.category}
+    </Text>
+    <Text
+      style={[styles.title, { color: textColor, marginBottom: 10, marginTop: 10, marginLeft: 15, marginRight: 15, fontSize: 20 }]}>
       {item.message}
     </Text>
 
-  
+
   </View>
 );
 
@@ -55,57 +63,55 @@ const ForumScreen = () => {
   useEffect(() => {
     const db = getFirestore();
     const discussionForumQuery = query(collection(db, "discussionForum"), orderBy("date", "desc"))
-    return onSnapshot(discussionForumQuery,(snapshot) => {
+    return onSnapshot(discussionForumQuery, (snapshot) => {
       const postData = [];
-      snapshot.forEach((doc) => postData.push({... doc.data() }));
+      snapshot.forEach((doc) => postData.push({ ...doc.data() }));
       setItems(postData);
     })
-    }, [])
+  }, [])
 
-  const renderItem = ({item}) => {
+  const renderItem = ({ item }) => {
     const backgroundColor = item.id === selectedId ? '#6495ed' : '#6495ed';
     const color = item.id === selectedId ? '#D9DBE0' : '#D9DBE0';
-    
+
     return (
-     
+
       <Item
         item={item}
         backgroundColor={backgroundColor}
         textColor={color}
       />
-     
+
     );
   };
 
   return (
-    
+
     <SafeAreaView style={styles.container}>
       <FlatList style={styles.popularItems}
         data={items}
         renderItem={renderItem}
         keyExtractor={item => item.id}
         extraData={selectedId}
+        ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
       />
 
     </SafeAreaView>
-    
+
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor:"#0A1419",
+    backgroundColor: "#0A1419",
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0, 
+    marginTop: StatusBar.currentHeight || 0,
   },
   popularItems: {
-    borderWidth: 0
+    borderWidth: 0,
   },
   item: {
     padding: 2,
-    marginVertical: 8,
-    marginHorizontal: 0,
-    borderRadius: 5,
     alignContent: 'center',
   },
   title: {
