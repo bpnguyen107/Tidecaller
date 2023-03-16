0204f11e94ff144406e4dddfc7e7921cbf751390
Sebastian Caid <sebicaid12@gmail.com>
2023-03-16 15:55:28 -0700

    Added functionality to remove and add favorites from the favorites page.

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -3,15 +3,35 @@ import { Text, View } from 'react-native'
 import { favArray } from '../screens/MapScreen.js'
 import { FontAwesome } from '@expo/vector-icons';
 
+var updatedFavorites = [];
+export function updateFav(){
+  return updatedFavorites;
+}
+
 const FavoriteScreen = () => {
 
   const [favorites, setFavorites] = useState([]);
+  const [favOriginal, setFavOriginal] = useState([]);
   const [clicked, setClicked] = useState(new Map());
 
+  function removeFav(i){
+    updatedFavorites.splice(i, 1);
+  }
+
+  function addFav(name, i){
+    if (i === updatedFavorites.length){
+      updatedFavorites.push(name);
+    }
+    else {
+      updatedFavorites.splice(i, 0, name);
+    }
+  }
 
 	useEffect(()=>{
     var fav = favArray();
 		setFavorites(fav);
+    setFavOriginal([...fav]);
+    updatedFavorites = [...fav];
     var clickedTemp = new Map();
     console.log("favorites length:", fav.length)
     for (let i = 0; i < fav.length; i++){
@@ -35,11 +55,14 @@ const FavoriteScreen = () => {
             var clickedTemp = new Map([...clicked]);
             if (clickedTemp.get(i) === true){
               clickedTemp.set(i, false);
+              removeFav(i);
             }
             else {
               clickedTemp.set(i, true);
+              addFav(favOriginal[i], i);
             }
             setClicked(clickedTemp);
+            console.log(updatedFavorites);
           }}
         />
       </Text>
